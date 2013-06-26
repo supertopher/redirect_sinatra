a2ba8d590c5862627466cbf0fc2a4eee2f5e8ad3
dbc-warrior <apprentice@devbootcamp.com>
2013-06-26 11:22:32 -0700

    Seeded that shit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@ require 'faker'
 30.times do Url.create(full_url: Faker::Internet.domain_name, 
                         click_counter: 0)
 end
+
+30.times do User.create(name: Faker::Name.name, 
+        email: Faker::Internet.email, password: "1234")
+end
